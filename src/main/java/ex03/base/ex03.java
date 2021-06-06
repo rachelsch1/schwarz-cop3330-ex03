a03c88346e6b0f87371cff71d862dad7f3e3d89b
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 12:00:04 -0400

    initial solution

--- src/main/java/ex03/base/ex03.java
+++ src/main/java/ex03/base/ex03.java
@@ -1,4 +1,19 @@
 package ex03.base;
 
+import java.util.Scanner;
+
 public class ex03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the quote? ");
+        String quote = input.nextLine();
+
+        System.out.print("Who said it? ");
+        String author = input.nextLine();
+
+        String outputString = author + " says, \"" + quote + "\"";
+
+        System.out.println(outputString);
+    }
 }
